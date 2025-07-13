1f9c0952285f58626bf6e4148baee6a0376e060f
Yurii M <ispoyar@yandex.ru>
2025-07-13 10:28:40 +0300

    added todo mock

---
+++ mocks/todos.ts
@@ -0,0 +1,29 @@
+import { TTodo } from "@/types";
+
+export const todos: TTodo[] = [
+  {
+    id: "0414c7de-cd2a-403e-ba10-1350b0f9c34e",
+    label: "Встать в 7:45",
+    completed: true,
+  },
+  {
+    id: "2d6827c9-0fdb-43d4-9a23-563e99d30585",
+    label: "Погладить кота",
+    completed: true,
+  },
+  {
+    id: "ba666a3f-5759-470b-9e15-1ac2f05aaf3e",
+    label: "Позавтракать и покормить кота",
+    completed: false,
+  },
+  {
+    id: "b190d9aa-89b3-4b94-ac64-39698ba56739",
+    label: "Заправить кровать",
+    completed: false,
+  },
+  {
+    id: "71e47481-3f97-4a70-a5e2-66519b3d137e",
+    label: "Заработать денег",
+    completed: false,
+  },
+];
