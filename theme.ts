3c9888a04532776e803f3399650613dc8a6dab3b
Yurii M <ispoyar@yandex.ru>
2025-07-13 10:26:51 +0300

    fixed mantine theme

--- theme.ts
+++ theme.ts
@@ -3,5 +3,31 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  fontFamily: "montserrat, sans-serif",
+  primaryColor: "gray",
+  headings: {
+    fontFamily: "montserrat",
+    sizes: {
+      h1: {
+        fontSize: "2rem",
+        fontWeight: "600",
+      },
+      h2: {
+        fontSize: "1.8rem",
+        fontWeight: "600",
+      },
+      h3: {
+        fontSize: "1.6rem",
+        fontWeight: "600",
+      },
+    },
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "nav-link-md": "2rem",
+  },
 });
