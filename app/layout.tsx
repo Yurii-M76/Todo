b244be419d18047160b38e450a3071db6cf8a871
Yurii M <ispoyar@yandex.ru>
2025-07-28 15:09:38 +0300

    added import mantine date styles to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Montserrat } from "next/font/google";
 import { MantineProvider, mantineHtmlProps } from "@mantine/core";
 import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
 import "@/styles/global.css";
 import { theme } from "../theme";
 import { ThemeToggler } from "@/components";
