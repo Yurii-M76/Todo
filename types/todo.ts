fa6689b6be852df49491313ad8ec0fca20a6c1ec
Yurii M <ispoyar@yandex.ru>
2025-07-31 16:45:33 +0300

    update todo types

--- types/todo.ts
+++ types/todo.ts
@@ -1,7 +1,10 @@
 export type TTodo = {
   id: string;
   label: string;
+  date: string | null;
+  time: string | null;
   completed: boolean;
 };
 
+export type TTodoCreate = Pick<TTodo, "label" | "date" | "time">;
 export type TTodoFiltered = "all" | "active" | "completed";
