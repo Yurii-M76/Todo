c120be836ad9fab57c3773f27caca8240caa97e0
Yurii M <ispoyar@yandex.ru>
2025-07-31 17:50:13 +0300

    update icon type

--- types/icon.ts
+++ types/icon.ts
@@ -1,7 +1,10 @@
+import { CSSProperties } from "react";
+
 export type TIcon = {
   width?: number;
   height?: number;
   fill?: string;
   stroke?: string;
   strokeWidth?: string;
+  style?: CSSProperties | undefined;
 };
